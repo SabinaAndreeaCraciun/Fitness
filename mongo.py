a51f5f31784d9d7f6d54e3a2f765b829f2f8a2f5
SabinaAndreeaCraciun <sabinaandreeacraciun@iesmontsia.org>
2025-05-16 12:28:09 +0200

    Modificat els comentaris

--- mongo.py
+++ mongo.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from datetime import datetime
+from bson import ObjectId
 
 # Connexió a MongoDB (modifica la URI segons el teu entorn)
 client = MongoClient("mongodb+srv://sabinaandreeacraciun:sabina1234@sabinadb.fsxnd.mongodb.net/")
@@ -39,7 +40,7 @@ def afegir_progres(usuari_id, exercici, series, repeticions):
 
     print(f"Progrés afegit per a l'usuari {usuari_id}")
     
-def afegir_comentari(usuari_id, comentari, ejercicio_id):
+def afegir_comentari(usuari_id, comentari, ejercicio_id=None):
     data_actual = datetime.now().strftime("%Y-%m-%d")
 
     if not comentari:
@@ -48,9 +49,10 @@ def afegir_comentari(usuari_id, comentari, ejercicio_id):
 
     nou_comentari = {
         "data": data_actual,
-        "comentari": comentari,
-        "ejercicio_id": ejercicio_id  # Guardamos el ID del ejercicio relacionado
+        "comentari": comentari
     }
+    if ejercicio_id:
+        nou_comentari["ejercicio_id"] = ejercicio_id  # Guardamos el ID del ejercicio relacionado
 
     usuari = col_comentaris.find_one({"usuari_id": usuari_id})
 
@@ -68,12 +70,18 @@ def afegir_comentari(usuari_id, comentari, ejercicio_id):
 
     print(f"Comentari afegit per a l'usuari {usuari_id}")
 
+def convertir_objectid_a_str(comentaris):
+    for comentari in comentaris:
+        for key, valor in comentari.items():
+            if isinstance(valor, ObjectId):
+                comentari[key] = str(valor)
+    return comentaris
+
 def obtenir_comentaris(usuari_id):
     usuari = col_comentaris.find_one({"usuari_id": usuari_id})
-    return usuari.get("notes", []) if usuari else []
-
-
-  
+    notes = usuari.get("notes", []) if usuari else []
+    notes = convertir_objectid_a_str(notes)
+    return notes
 
 def guardar_estadistiques(usuari_id, temps_total, calories_totals):
     if temps_total is None or calories_totals is None:
@@ -94,10 +102,8 @@ def guardar_estadistiques(usuari_id, temps_total, calories_totals):
 
     print(f"Estadístiques guardades per a l'usuari {usuari_id}")
 
-
 # Exemple d’ús
 if __name__ == "__main__":
     afegir_progres(usuari_id=2, exercici="Curl", series=4, repeticions=10)
-
     afegir_comentari(usuari_id=2, comentari="He augmentat pes en esquats.")
     guardar_estadistiques(usuari_id=2, temps_total=240, calories_totals=15000)
